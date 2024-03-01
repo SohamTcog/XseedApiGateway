957fe0383f5105ab9be524e62ac68f80c8834657
SohamTcog <soham.kulkarni@tcognition.com>
2024-03-01 10:15:30 +0530

    test file changed

---
+++ src/test/java/com/xseedApi/XseedApiGatewayApplicationTests.java
@@ -0,0 +1,10 @@
+package com.xseedApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class XseedApiGatewayApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
