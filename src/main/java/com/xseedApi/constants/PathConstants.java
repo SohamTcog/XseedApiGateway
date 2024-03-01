a5e7527eaa1f30e4fb2c98c6e548969394b20bc6
SohamTcog <soham.kulkarni@tcognition.com>
2024-03-01 11:09:30 +0530

    constants

--- src/main/java/com/xseedApi/constants/PathConstants.java
+++ src/main/java/com/xseedApi/constants/PathConstants.java
@@ -4,4 +4,5 @@ public class PathConstants {
     public static final String RECRUITER_PATH = "api/jobcreation";
     public static final String RECRUITER_LISTING_PATH = "/job/joblisting";
     public static final String ADMIN_PATH = "/job/admin";
+    public static final String TEMPLATE_PATH= "/api/template";
 }
